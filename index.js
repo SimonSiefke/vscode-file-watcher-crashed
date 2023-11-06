49f195b3ae4c932ee5d71d27b6d1aa073415f99b
Simon Siefke <simon.siefke@gmail.com>
2023-11-06 17:44:40 +0100

    disable extensions

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { spawn } from "node:child_process";
 export const downloadAndUnzipVscode = async (vscodeVersion) => {
   const { downloadAndUnzipVSCode } = await import("@vscode/test-electron");
   const path = await downloadAndUnzipVSCode(vscodeVersion);
-  spawn(path, ["--user-data-dir", "/tmp"], {
+  spawn(path, ["--user-data-dir", "/tmp", "--disable-extensions"], {
     stdio: "inherit",
   });
 };
